36c61e3671d7e642257446e9ea79226693cad565
Дмитрий <DmitrySoloviev>
2022-08-15 23:16:49 +0300

    changed data_dao.py

--- DAO/data_dao.py
+++ DAO/data_dao.py
@@ -11,3 +11,20 @@ class DataDAO:
 
     def get_data(self):
         return self._load_data()
+
+    def get_posts_by_user(self, user_name):
+        list_posts = self._load_data()
+        user_posts = []
+        names_of_users = []
+        for posts in list_posts:
+            names_of_users.append(posts['poster_name'].lower())
+        if user_name.lower() in names_of_users:
+            pass
+        else:
+            user_name = "leo"
+        for posts in list_posts:
+            if posts['poster_name'].lower() == user_name.lower():
+                user_posts.append(posts)
+            else:
+                pass
+        return user_posts
