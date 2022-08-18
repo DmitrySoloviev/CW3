e6153c214c683d027b69db39e8960283c6e363d6
Дмитрий <DmitrySoloviev>
2022-08-18 16:50:47 +0300

    Changed app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import logging
-from flask import Flask, request, render_template, abort
 from main.main import main_page
 from utils import get_post_by_pk, get_comments_by_post_id, search_for_posts, get_posts_by_user
-from flask_restful import Api, Resource, reqparse
-
+from flask import Flask, jsonify, abort, request, render_template
+from DAO.data_dao import DataDAO
+from DAO.comments_dao import CommentsDAO
 
 app = Flask(__name__)
-api = Api(app)
+app.config['JSON_AS_ASCII'] = False
 
 # Регистрация blueprint
 app.register_blueprint(main_page)
@@ -45,9 +45,52 @@ def page_user(user_name):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('index.html'), 404
+    return render_template('error.html'), 404
 
+path = "data/data.json"
+path_two = "data/comments.json"
+data_dao = DataDAO(path)
+comments_dao = CommentsDAO(path_two)
+
+@app.route("/api")
+def data_page():
+    data = data_dao.get_data()
+    return jsonify(data)
+
+
+@app.route("/api/posts/<int:post_id>")
+def single_page_api(post_id):
+    posts = data_dao.get_post_by_pk(post_id)
+    comments_for_post = comments_dao.get_comments_by_post_id(post_id)
+    complete_post = []
+    for post in posts:
+        complete_post.append(post)
+    for comment in comments_for_post:
+        complete_post.append(comment)
+    if posts is None or comments_for_post is None:
+        return abort(404)
+    return jsonify(comments_for_post)
+
+
+@app.route("/api/search")
+def search_page_api():
+    s = request.args['s']
+    post_with = data_dao.search_for_posts(s)
+    if post_with is None:
+        return abort(404)
+    return jsonify(post_with)
+
+
+@app.route("/api/users/<user_name>")
+def page_user_api(user_name):
+    user_posts = data_dao.get_posts_by_user(user_name)
+    if user_posts is None:
+        return abort(404)
+    return jsonify(user_posts)
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
 
 # Запуск Flask
-app.run(port=8000, debug=True)
+#app.run(port=8000, debug=True)
 
